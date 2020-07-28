7a910cd0e713fc8f65be5b27f885c784a7debc9a
EvanHendersonRichtByte <adamhenderson3x3@gmail.com>
2020-07-28 08:08:41 +0700

    update ticket migration

---
+++ database/migrations/2020_07_27_190038_create_ticket_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket', function (Blueprint $table) {
+            $table->increments('id_ticket');
+            $table->string('email');
+            $table->string('ticket_subject');
+            $table->string('ticket_category')->nullable();
+            $table->text('ticket_body');
+            $table->string('priority');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket');
+    }
+}
