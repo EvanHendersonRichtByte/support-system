ea0d96dbdb1f079c79c6026f9c97b8a2ab20f3cd
BosToken <faizdiandra11@gmail.com>
2020-08-05 08:09:31 +0700

    update client+controller

--- resources/views/admin/client.blade.php
+++ resources/views/admin/client.blade.php
@@ -56,9 +56,7 @@ Client List
                         <button class="popupformWR btn btn-success btn-lg apopf added-ripples" data-toggle="modal"
                             data-target="#loginModal{{$d->id}}" data-effect="mfp-move-from-top"> <span>Detail</span>
                         </button>
-                        <button class="popupformWR btn btn-info btn-lg apopf added-ripples" data-toggle="modal"
-                            data-target="#editModal{{$d->id}}" > <span>Edit 2</span>
-                        </button>
+                       
                         <a href="/client/edit/{{$d->id}}" class="btn btn-warning btn-sm">Edit</a>
                     <td>
                         <div class="modal fade" id="loginModal{{$d->id}}" tabindex="-1" role="dialog"
@@ -131,78 +129,7 @@ Client List
                                     </div>
                             </form>
                         </div>
-                        <div class="modal fade" id="editModal{{$d->id}}" tabindex="-1" role="dialog"
-                            aria-labelledby="f" aria-hidden="true">
-                            <form action="{{url('check_login')}}" method="POST">
-                                {{ csrf_field() }}
-                                <div class="modal-dialog modal-dialog-centered" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h4 class="modal-title">Client Edit
-                                            </h4>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <div class="row">
-                                                <div class="col-xs-12">
-                                                    <div class="form-group">
-                                                        Email
-                                                        <input id="password" type="email" class="form-control"
-                                                            name="password" value="{{$d->email}}" required autofocus>
-
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-xs-12 ">
-                                                    <div class="form-group">
-                                                        Password
-                                                        <input id="password" type="text" class="form-control"
-                                                            name="password" value="{{$d->email}}" required autofocus>
-
-
-
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-xs-12 ">
-                                                    <div class="form-group">
-                                                        Username
-                                                        <input id="password" type="text" class="form-control"
-                                                            name="password" value="{{$d->username}}" required autofocus>
-
-
-
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-xs-12 ">
-                                                    <div class="form-group">
-                                                        Created At
-                                                        <input id="password" type="text" class="form-control"
-                                                            name="password" value="{{$d->created_at}}" required
-                                                            autofocus>
-
-
-
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="modal-footer ">
-                                                <button type="button" class="btn btn-secondary"
-                                                    data-dismiss="modal">Close</button>
-                                                <button type="submit" type="button"
-                                                    class="btn btn-primary">Submit</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                            </form>
-                        </div>
+                        
                 <tbody>
                     </tr>
                     @endforeach
